3a967675602154a4d9708e07c40185bdd679ccf7
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2018-04-02 21:58:01 +0900

    add css sample

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
 import Link from 'next/link'
-export default () => <div>
-  <div>Welcome to next.js!</div>
-  <Link href="/about">about</Link>
-</div>
+export default () =>
+  <div>
+    <div>Welcome to next.js!</div>
+    <Link href="/about">about</Link>
+    <p>scoped!</p>
+    <style jsx>{`
+      p {
+        color: blue;
+      }
+      div {
+        background: red;
+      }
+      @media (max-width: 600px) {
+        div {
+          background: blue;
+        }
+      }
+    `}</style>
+    <style global jsx>{`
+      body {
+        background: black;
+      }
+    `}</style>
+  </div>
