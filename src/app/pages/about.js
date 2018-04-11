6133a60a40076bf42406c1dee305b411e6a8d024
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2018-04-11 23:51:46 +0900

    header link test

--- src/app/pages/about.js
+++ src/app/pages/about.js
@@ -1,7 +1,10 @@
 import React from "react"
 import Link from 'next/link'
+import Header from '../components/header'
+
 export default () => (
   <div>
+    <Header/>
     <div>About Page</div>
     <Link href="/index"><a>index</a></Link>
   </div>
